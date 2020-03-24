35a23c369fda365a5020e2373f355234324870ce
Brady Snuggs <dronebug360@gmail.com>
2020-03-24 00:14:58 -0600

    isSingleLine function.

--- search.js
+++ search.js
@@ -11,7 +11,7 @@ module.exports = function search (grid, wordlist) {
     /*
     * Additional Important Notes from Email Exchange with Jake, instructions, and looking at sample Grids/WordLists:
         1) Words are formed in a single, contiguous direction of characters (more like a crossword than a boggle game). Keep track of direction.
-        2) Resulting array of words does not need to account for duplicates. If it exists in the grid and wordlist in a single, contiguous direction, return it.
+        2) Resulting array of words does not need to account for duplicates. If it exists in the grid and wordlist in a single, contiguous direction, return it. "You only need to find words that lie in a straight line."
         3) Given the example grids, it seems safe to assume that characters can be reused to form other words from previously found words. For example, 'SUN' and 'MOON' share the same 'N' in grid2. 'LOVE' and 'LION' share the same 'L' in grid1.
         4) Case insensitive. Ensure values that are being checked are either both lowercase/uppercase. The wordlist seems to always be lowercase based on examples provided in sample.
     */
@@ -29,22 +29,67 @@ module.exports = function search (grid, wordlist) {
         }
     }
 
-    console.log(wordlistNoDuplicates)
-    console.table(grid)
+    console.log(wordlistNoDuplicates);
+    console.table(grid);
     return Object.keys(wordsFound);
 }
 
-function checkCharAtCoordinateAgainstCharInWords(row, column, grid, trieNode, wordsFound){
-    const letter = grid[row][column].toLowerCase()
+function checkCharAtCoordinateAgainstCharInWords(row, column, grid, trieNode, wordsFound, coordinates=[]){
+    const letter = grid[row][column].toLowerCase();
     if(!(letter in trieNode)) return;
+    //Need to see if direction is consistent - Need at least three coordinates to test
+    if(coordinates.length >= 3){
+        if(!isStraightLine(coordinates)) return coordinates=[];
+    }
+    coordinates.push([row, column])
     trieNode = trieNode[letter];
     if('*' in trieNode){
+        console.log(trieNode)
         wordsFound[trieNode['*']] = true;
+        coordinates=[];
     };
     const adjacentCharacters = getAdjacentCharacters(row, column, grid);
     for(const adjacentCharacter of adjacentCharacters){
-        checkCharAtCoordinateAgainstCharInWords(adjacentCharacter[0], adjacentCharacter[1], grid, trieNode, wordsFound)
+        checkCharAtCoordinateAgainstCharInWords(adjacentCharacter[0], adjacentCharacter[1], grid, trieNode, wordsFound, coordinates);
     };
+}
+
+function isStraightLine(coordinates){
+    let isSingleLine = true;
+
+    const south = coordinates[0][0] < coordinates[1][0] && coordinates[0][1] === coordinates[1][1],
+        north = coordinates[0][0] > coordinates[1][0] && coordinates[0][1] === coordinates[1][1],
+        northEast = coordinates[0][0] > coordinates[1][0] && coordinates[0][1] < coordinates[1][1],
+        northWest = coordinates[0][0] > coordinates[1][0] && coordinates[0][1] > coordinates[1][1],
+        east = coordinates[0][0] === coordinates[1][0] && coordinates[0][1] < coordinates[1][1],
+        west = coordinates[0][0] === coordinates[0][1] && coordinates[0][1] > coordinates[1][1],
+        southEast = coordinates[0][1] > coordinates[1][0] && coordinates[0][1] < coordinates[1][1],
+        southWest = coordinates[0][1] > coordinates[1][0] && coordinates[0][1] > coordinates[1][1];
+    
+    for(const coordinate of coordinates){
+        if(south){
+            coordinate[]
+        } else if(southEast){
+
+        } else if(southWest){
+
+        } else if(north){
+
+        } else if(northEast){
+
+        } else if(northWest){
+
+        } else if(east){
+
+        } else if(west){
+
+        } 
+    }
+
+    console.log({south, north, northEast, northWest, east, west, southEast, southWest})
+
+
+    return isSingleLine;
 }
 
 function getAdjacentCharacters(row, column, grid){
@@ -94,16 +139,16 @@ class Trie{
     constructor(){
         this.root={};
         this.endSymbol='*';
-    }
+    };
 
     add(word){
         let current = this.root;
         for(const letter of word){
             if(!(letter in current)) {
                 current[letter] = {};
-            }
+            };
             current = current[letter];
-        }
+        };
         current[this.endSymbol] = word;
-    }
+    };
 }
