924ce52079b05300d650eb0ce4a0b160462a8489
Brady Snuggs <dronebug360@gmail.com>
2020-03-24 10:24:26 -0600

    commenting out isStraightLine for now until I can think of a more efficient solution.

--- search.js
+++ search.js
@@ -34,69 +34,55 @@ module.exports = function search (grid, wordlist) {
     return Object.keys(wordsFound);
 }
 
-function checkCharAtCoordinateAgainstCharInWords(row, column, grid, trieNode, wordsFound, coordinates=[]){
+
+function checkCharAtCoordinateAgainstCharInWords(row, column, grid, trieNode, wordsFound){
     const letter = grid[row][column].toLowerCase();
     if(!(letter in trieNode)) return;
     trieNode = trieNode[letter];
-    // coordinates.push([row, column]);
-    // if(coordinates.length >= 3){
-    //     if(isStraightLine(coordinates)) {
-    //         // console.log(trieNode, grid, letter, coordinates)
-    //         if('*' in trieNode){
-    //             console.log(trieNode, grid, 'finished');
-    //             wordsFound[trieNode['*']] = true;
-    //             coordinates=[];
-    //         };
-    //     } else {
-    //         coordinates.pop()
-    //     }
-    // };
     if('*' in trieNode){
+        console.log(coordinates, wordsFound, trieNode)
+        console.table(grid)
         wordsFound[trieNode['*']] = true;
-        coordinates=[];
-    };
+    } 
     const adjacentCharacters = getAdjacentCharacters(row, column, grid);
     for(const adjacentCharacter of adjacentCharacters){
-        checkCharAtCoordinateAgainstCharInWords(adjacentCharacter[0], adjacentCharacter[1], grid, trieNode, wordsFound, coordinates);
+        checkCharAtCoordinateAgainstCharInWords(adjacentCharacter[0], adjacentCharacter[1], grid, trieNode, wordsFound);
     };
 }
 
-function isStraightLine(coordinates){
-    let isAStraightLine = true;
-
-    let south = coordinates[0][0] < coordinates[1][0] && coordinates[0][1] === coordinates[1][1],
-        north = coordinates[0][0] > coordinates[1][0] && coordinates[0][1] === coordinates[1][1],
-        northEast = coordinates[0][0] > coordinates[1][0] && coordinates[0][1] < coordinates[1][1],
-        northWest = coordinates[0][0] > coordinates[1][0] && coordinates[0][1] > coordinates[1][1],
-        east = coordinates[0][0] === coordinates[1][0] && coordinates[0][1] < coordinates[1][1],
-        west = coordinates[0][0] === coordinates[0][1] && coordinates[0][1] > coordinates[1][1],
-        southEast = coordinates[0][1] > coordinates[1][0] && coordinates[0][1] < coordinates[1][1],
-        southWest = coordinates[0][1] > coordinates[1][0] && coordinates[0][1] > coordinates[1][1];
+// function isStraightLine(coordinates){
+//     let isAStraightLine = true;
+//     let south = coordinates[0][0] < coordinates[1][0] && coordinates[0][1] === coordinates[1][1],
+//         north = coordinates[0][0] > coordinates[1][0] && coordinates[0][1] === coordinates[1][1],
+//         northEast = coordinates[0][0] > coordinates[1][0] && coordinates[0][1] < coordinates[1][1],
+//         northWest = coordinates[0][0] > coordinates[1][0] && coordinates[0][1] > coordinates[1][1],
+//         east = coordinates[0][0] === coordinates[1][0] && coordinates[0][1] < coordinates[1][1],
+//         west = coordinates[0][0] === coordinates[0][1] && coordinates[0][1] > coordinates[1][1],
+//         southEast = coordinates[0][1] > coordinates[1][0] && coordinates[0][1] < coordinates[1][1],
+//         southWest = coordinates[0][1] > coordinates[1][0] && coordinates[0][1] > coordinates[1][1];
     
-    for(let i = 2; i < coordinates.length; i++){
-        if(south){
-            south = coordinates[i-1][0] < coordinates[i][0] && coordinates[i-1][1] === coordinates[i][1]
-        } else if(southEast){
-            southEast = coordinates[i-1][1] > coordinates[i][0] && coordinates[i-1][1] < coordinates[i][1]
-        } else if(southWest){
-            southWest = coordinates[i-1][1] > coordinates[i][0] && coordinates[i-1][1] > coordinates[i][1]
-        } else if(north){
-            north = coordinates[i-1][1] > coordinates[i][0] && coordinates[i-1][1] > coordinates[i][1]
-        } else if(northEast){
-            northEast = coordinates[i-1][1] > coordinates[i][0] && coordinates[i-1][1] > coordinates[i][1]
-        } else if(northWest){
-            northWest = coordinates[i-1][1] > coordinates[i][0] && coordinates[i-1][1] > coordinates[i][1]
-        } else if(east){
-            east = coordinates[i-1][1] > coordinates[i][0] && coordinates[i-1][1] > coordinates[i][1]
-        } else if(west){
-            west = coordinates[i-1][1] > coordinates[i][0] && coordinates[i-1][1] > coordinates[i][1]
-        } 
-    }
-    console.log(coordinates)
-    console.log(south || north || northEast || northWest || east || west || southEast || southWest);
+//     for(let i = 2; i < coordinates.length; i++){
+//         if(south){
+//             south = coordinates[i-1][0] < coordinates[i][0] && coordinates[i-1][1] === coordinates[i][1]
+//         } else if(southEast){
+//             southEast = coordinates[i-1][1] > coordinates[i][0] && coordinates[i-1][1] < coordinates[i][1]
+//         } else if(southWest){
+//             southWest = coordinates[i-1][1] > coordinates[i][0] && coordinates[i-1][1] > coordinates[i][1]
+//         } else if(north){
+//             north = coordinates[i-1][1] > coordinates[i][0] && coordinates[i-1][1] > coordinates[i][1]
+//         } else if(northEast){
+//             northEast = coordinates[i-1][1] > coordinates[i][0] && coordinates[i-1][1] > coordinates[i][1]
+//         } else if(northWest){
+//             northWest = coordinates[i-1][1] > coordinates[i][0] && coordinates[i-1][1] > coordinates[i][1]
+//         } else if(east){
+//             east = coordinates[i-1][1] > coordinates[i][0] && coordinates[i-1][1] > coordinates[i][1]
+//         } else if(west){
+//             west = coordinates[i-1][1] > coordinates[i][0] && coordinates[i-1][1] > coordinates[i][1]
+//         } 
+//     }
 
-    return south || north || northEast || northWest || east || west || southEast || southWest;
-}
+//     return south || north || northEast || northWest || east || west || southEast || southWest;
+// }
 
 function getAdjacentCharacters(row, column, grid){
     const adjacentCharacters = [];
